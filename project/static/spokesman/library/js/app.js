19f41276ba2ab77e12f89ef91478db544ac987ef
Joel Smith <smithjoelp@gmail.com>
2020-04-21 21:27:37 -0700

    Happy with the journo-list spacing with and without js, at mobile and responsive

--- project/static/spokesman/library/js/app.js
+++ project/static/spokesman/library/js/app.js
@@ -8,8 +8,10 @@ var jpsJournoList = (function(){
         DOM.navMenu = document.getElementById('journo-list-menu');
         DOM.navMenuItems = document.querySelectorAll('#journo-list-menu li');
         DOM.navMenuLinks = document.querySelectorAll('#journo-list-menu a');
-        DOM.journoList = document.getElementById('#journo-list');
+        DOM.journoList = document.getElementById('journo-list');
         DOM.journoListItems = document.querySelectorAll('#journo-list li');
+        DOM.firstJournoListItem = document.querySelector('#journo-list li');
+        DOM.jsHide = document.querySelectorAll('.js-hide');
     }
 
     function bindEvents(){
@@ -19,8 +21,14 @@ var jpsJournoList = (function(){
     }
 
     function setUp(){
+        DOM.firstJournoListItem.classList.add('active');
         DOM.body.classList.add('js-enabled');
         addImgsToNav();
+        // setContainerMinHeight();
+        DOM.jsHide.forEach(function(item){
+            item.classList.add('vh');
+        });
+
     }
 
     function addImgsToNav(){
@@ -36,12 +44,55 @@ var jpsJournoList = (function(){
         e.preventDefault();
         DOM.journoListItems.forEach(function(item){
             item.style.display = 'none';
+            item.classList.remove('active');
         });
         let targetHref = this.getAttribute('href');
         let target = document.getElementById(targetHref.slice(1));
-        target.style.display = 'block';
-        console.log(target);
+        fade('in',850,target);
+        target.classList.add('active');
+    }
+
+    function fade(type, ms, el) {
         
+        // set up initial variables
+        let isIn = type === 'in',
+            opacity = isIn ? 0 : 1,
+            interval = 50,
+            duration = ms,
+            gap = interval / duration;
+      
+        if(isIn) {
+            el.style.display = 'block';
+            el.style.opacity = opacity;
+        }
+      
+        function func() {
+            opacity = isIn ? opacity + gap : opacity - gap;
+            el.style.opacity = opacity;
+      
+            if(opacity <= 0) el.style.display = 'none'
+            if(opacity <= 0 || opacity >= 1) window.clearInterval(fading);
+        }
+      
+        var fading = window.setInterval(func, interval);
+      
+    }
+
+    function setContainerMinHeight(){
+        let height = 0;
+        let i = 0;
+        DOM.journoListItems.forEach(function(item){
+            item.style.display = 'block';
+            let isActive = item.classList.contains('active');
+            let thisHeight = item.offsetHeight;
+            height = thisHeight > height ? thisHeight : height;
+            if(!isActive){
+                item.style.display = 'none';
+            }
+        });
+        console.log("HEIGHT");
+        console.log(height);
+        DOM.journoList.style.height = height + 'px';
     }
     
     // main init method
--- project/utils/models.py
+++ project/utils/models.py
@@ -3,7 +3,6 @@ from django.conf import settings
 import os
 import json
 
-# Create your models here.
 class Importer(models.Model):
 
     class Meta:
